99f3d9dc8c8988eae7258ba473020ed3c84fb235
kermage <genealyson.torcende@gmail.com>
2020-01-27 12:35:54 +0800

    prefix our transient names

--- src/Cache.php
+++ src/Cache.php
@@ -11,23 +11,26 @@ namespace ThemePlate;
 
 class Cache {
 
+	private static $prefix = 'tpc_';
+
+
 	public static function get( $key ) {
 
-		return get_transient( $key );
+		return get_transient( self::$prefix . $key );
 
 	}
 
 
 	public static function set( $key, $value, $expiration ) {
 
-		return set_transient( $key, $value, $expiration );
+		return set_transient( self::$prefix . $key, $value, $expiration );
 
 	}
 
 
 	public static function delete( $key ) {
 
-		return delete_transient( $key );
+		return delete_transient( self::$prefix . $key );
 
 	}
 
