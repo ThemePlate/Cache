a7d1fce15af501c8daf437b1c45f6c5204e003db
kermage <genealyson.torcende@gmail.com>
2022-02-26 20:08:51 +0800

    3 meta type storages

--- src/StorageManager.php
+++ src/StorageManager.php
@@ -10,16 +10,25 @@ namespace ThemePlate\Cache;
 use ThemePlate\Cache\Storages\AbstractStorage;
 use ThemePlate\Cache\Storages\MetadataStorage;
 use ThemePlate\Cache\Storages\OptionsStorage;
+use ThemePlate\Cache\Storages\PostMetaStorage;
+use ThemePlate\Cache\Storages\TermMetaStorage;
+use ThemePlate\Cache\Storages\UserMetaStorage;
 
 class StorageManager {
 
 	private string $type = 'options';
 	private MetadataStorage $metadata;
+	private PostMetaStorage $postmeta;
+	private TermMetaStorage $termmeta;
+	private UserMetaStorage $usermeta;
 	private OptionsStorage $options;
 
 
 	public function __construct() {
 
+		$this->postmeta = new PostMetaStorage();
+		$this->termmeta = new TermMetaStorage();
+		$this->usermeta = new UserMetaStorage();
 		$this->metadata = new MetadataStorage( 'post' );
 		$this->options  = new OptionsStorage();
 
