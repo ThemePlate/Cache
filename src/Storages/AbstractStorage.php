d3fa7c8d0cf137193325eaed231aa5955ef126da
kermage <genealyson.torcende@gmail.com>
2022-05-26 23:35:40 +0800

    initialize the property value

--- src/Storages/AbstractStorage.php
+++ src/Storages/AbstractStorage.php
@@ -9,7 +9,7 @@ namespace ThemePlate\Cache\Storages;
 
 abstract class AbstractStorage implements StorageInterface {
 
-	protected array $collection;
+	protected array $collection = array();
 
 
 	protected function transform( string $field_key, bool $is_data ): string {
